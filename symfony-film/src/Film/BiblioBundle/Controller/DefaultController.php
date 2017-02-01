5f5bd6df249d10fccc361fda7aceda629cd7db85
Alanlgf <alanlegoff78@gmail.com>
2017-02-01 11:50:44 +0100

    Ajout du debut du projet

---
+++ symfony-film/src/Film/BiblioBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Film\BiblioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FilmBiblioBundle:Default:index.html.twig');
+    }
+}
---
+++ symfony-film/src/Film/BiblioBundle/FilmBiblioBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Film\BiblioBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FilmBiblioBundle extends Bundle
+{
+}
